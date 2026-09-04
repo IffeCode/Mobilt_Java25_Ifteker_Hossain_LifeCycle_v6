451811419e6349ce68ca34a26a0205e5a7cb5931
iffe <iffe.h@hotmail.com>
2026-09-04 10:10:23 +0200

    commit

--- app/src/main/java/com/iffecode/lifecycle_v6/MainActivity.java
+++ app/src/main/java/com/iffecode/lifecycle_v6/MainActivity.java
@@ -1,6 +1,7 @@
 package com.iffecode.lifecycle_v6;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        Button login = findViewById(R.id.LoginBtn);
 
 
 
