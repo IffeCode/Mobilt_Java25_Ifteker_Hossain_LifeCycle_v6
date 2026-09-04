b10aeb4636f7801f240924aa9dbcec6fe456e633
iffe <iffe.h@hotmail.com>
2026-09-04 11:36:30 +0200

    Added and removed fidget from mainactivity2 xml

--- app/src/main/java/com/iffecode/lifecycle_v6/MainActivity.java
+++ app/src/main/java/com/iffecode/lifecycle_v6/MainActivity.java
@@ -1,6 +1,8 @@
 package com.iffecode.lifecycle_v6;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -18,9 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        //
-
-
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -28,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
 
         Button login = findViewById(R.id.LoginBtn);
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+
+
+            }
+        });
 
 
 
